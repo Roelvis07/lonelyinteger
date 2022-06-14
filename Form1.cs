1f477c51e6c967264d4036ee163c46bdbee47764
Roelvis Coello <rcabijana@gmail.com>
2022-06-14 00:07:22 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lonelyinteger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<int> arr = new List<int>() { 1, 2, 3, 4, 3, 2, 1 };
+            Console.WriteLine(lonelyinteger(arr));
+        }
+        public static int lonelyinteger(List<int> a)
+        {
+            int donRepeat = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                int result = a.Count(f => f == a[i]);
+                if(result == 1)
+                {
+                    donRepeat = a[i];
+                    break;
+                }
+
+            }
+            return donRepeat;
+        }
+
+    }
+}
